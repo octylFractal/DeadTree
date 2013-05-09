2c628caecdce8ee2c27ac273ea7080bc36a9bc23
Kenzie Togami <ket1999@gmail.com>
2013-05-08 21:23:59 -0700

    Version system started
    This is version 1.0 alpha, contains only a centered window.

--- logixsim/src/k/logix/main/LogixMain.java
+++ logixsim/src/k/logix/main/LogixMain.java
@@ -1,9 +1,22 @@
 package k.logix.main;
 
+import javax.swing.JFrame;
+
+import k.logix.util.ScreenUtil;
+
 public class LogixMain {
 	
-	public static void main(String[] args) {
+	public static final String version = "1.0 alpha";
 
+	public static void main(String[] args) {
+		setupScreen("logixsim v"+version);
+	}
+
+	private static void setupScreen(String title) {
+		JFrame screen = new JFrame(title);
+		screen.setSize(800, 600);
+		ScreenUtil.center(screen);
+		screen.setVisible(true);
 	}
 
 }
---
+++ logixsim/src/k/logix/util/ScreenUtil.java
@@ -0,0 +1,11 @@
+package k.logix.util;
+
+import javax.swing.JFrame;
+
+public class ScreenUtil {
+
+	public static void center(JFrame frame) {
+		frame.setLocationRelativeTo(null);
+	}
+
+}
