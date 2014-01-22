00974f33539f322cac2e691ec445f7fe02671671
Kenzie Togami <ket1999@gmail.com>
2014-01-22 15:57:27 -0800

    Use the pwd instead of hax

--- logixsim/src/k/logix/util/FileUtil.java
+++ logixsim/src/k/logix/util/FileUtil.java
@@ -1,22 +1,11 @@
 package k.logix.util;
 
 import java.io.File;
-import java.net.URISyntaxException;
 
 public class FileUtil {
 
 	public static File getTop() {
-		File this_dir = null;
-		try {
-			this_dir = new File(FileUtil.class.getClassLoader()
-					.getResource("k/logix/util/FileUtil.class").toURI().getPath());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		File out = this_dir.getParentFile().getParentFile().getParentFile()
-				.getParentFile();
-		return out.getAbsoluteFile();
+		return new File("").getAbsoluteFile();
 	}
 
 }
