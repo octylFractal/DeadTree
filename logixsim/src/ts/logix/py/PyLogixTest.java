42be88964afed15b5dd7a61abec90394bd69b1fa
Kenzie Togami <ket1999@gmail.com>
2014-01-25 12:30:27 -0800

    Test gates

--- logixsim/src/ts/logix/py/PyLogixTest.java
+++ logixsim/src/ts/logix/py/PyLogixTest.java
@@ -1,5 +1,8 @@
 package ts.logix.py;
 
+import java.util.Arrays;
+
+import ts.logix.py.logicsys.Gate;
 import ts.logix.py.pointsys.PyPoint;
 
 public class PyLogixTest {
@@ -11,6 +14,18 @@ public class PyLogixTest {
         System.err.println(one.state() + "=" + two.state());
         one.pushState(State.ON);
         System.err.println(one.state() + "=" + two.state());
+        one.pushState(State.OFF);
+        logixmain__.disconnect(one, two);
+        Gate gate = logicsys.newGate("andGate").build();
+        logixmain__
+                .wireInputsToGate(Arrays.asList(new PyPoint[] { one }), gate);
+        logixmain__.wireGateToOutputs(gate,
+                Arrays.asList(new PyPoint[] { two }));
+        System.err.println(gate);
+        one.pushState(State.ON);
+        gate.doLogic();
+        System.err.println(gate);
+
     }
 
 }
