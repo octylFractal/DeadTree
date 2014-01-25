38462575c29a6aaeddcba658542051e333ba3f49
Kenzie Togami <ket1999@gmail.com>
2014-01-25 13:37:08 -0800

    Document GateSys

--- logixsim/src/ts/logix/py/GateSys.java
+++ logixsim/src/ts/logix/py/GateSys.java
@@ -7,6 +7,11 @@ import java.util.List;
 
 import ts.logix.py.PointSys.PyPoint;
 
+/**
+ * The matching Python file is named logicsys.py.
+ * 
+ * @author Kenzie Togami
+ */
 public class GateSys {
 
     /**
@@ -14,14 +19,42 @@ public class GateSys {
      */
     public static final int VARIABLE_COUNT = -1;
 
+    /**
+     * The map of ids to {@link GateInfo}.
+     */
     private static HashMap<String, GateInfo> gates = new HashMap<String, GateInfo>();
 
+    /**
+     * The map of {@link Gate} classes to the respective {@link GateInfo
+     * GateInfos}.
+     */
     private static HashMap<Class<? extends Gate>, GateInfo> classToGate = new HashMap<Class<? extends Gate>, GateInfo>();
 
+    /**
+     * Returns a new {@link GateBuilder} to create {@link Gate Gates} with.
+     * 
+     * @param id
+     *            - the id of the gate to create
+     * @return a new GateBuilder with the gate id <tt>id</tt>.
+     */
     public static GateBuilder newGate(String id) {
         return new GateBuilder(id);
     }
 
+    /**
+     * Registers this gate with the internal system.
+     * 
+     * @param gCls
+     *            - the gate's class
+     * @param gId
+     *            - the gate's identifier
+     * @param inputCount
+     *            - the number of inputs this accepts, or
+     *            {@link GateSys#VARIABLE_COUNT} for any number of inputs.
+     * @param outputCount
+     *            - the number of outputs this accepts, or
+     *            {@link GateSys#VARIABLE_COUNT} for any number of outputs.
+     */
     public static void addGate(Class<? extends Gate> gCls, String gId,
             int inputCount, int outputCount) {
         GateInfo gi = new GateInfo(gCls, gId, inputCount, outputCount);
@@ -29,24 +62,60 @@ public class GateSys {
         classToGate.put(gCls, gi);
     }
 
+    /**
+     * A class for building {@link Gate Gates}.
+     * 
+     * @author Kenzie Togami
+     */
     public static class GateBuilder {
+        /**
+         * The id for the gate.
+         */
         private String gid;
+        /**
+         * The number of inputs/outputs used by the created gate.
+         */
         private int icount = 1, ocount = 1;
 
+        /**
+         * Creates a new GateBuilder with the specified id.
+         * 
+         * @param id
+         *            - the gate id
+         */
         public GateBuilder(String id) {
             gid = id;
         }
 
+        /**
+         * Sets the number of inputs to create the gate with.
+         * 
+         * @param count
+         *            - the number of inputs
+         * @return <tt>this</tt>
+         */
         public GateBuilder setInputs(int count) {
             icount = count;
             return this;
         }
 
+        /**
+         * Sets the number of outputs to create the gate with.
+         * 
+         * @param count
+         *            - the number of outputs
+         * @return <tt>this</tt>
+         */
         public GateBuilder setOutputs(int count) {
             ocount = count;
             return this;
         }
 
+        /**
+         * Constructs the {@link Gate} and performs checks.
+         * 
+         * @return a new {@link Gate}
+         */
         public Gate build() {
             GateInfo gi = gates.get(gid);
             if (gi.icount != VARIABLE_COUNT && icount != gi.icount) {
@@ -66,12 +135,43 @@ public class GateSys {
         }
     }
 
+    /**
+     * Holds info about a {@link Gate} class.
+     * 
+     * @author Kenzie Togami
+     */
     private static class GateInfo {
+        /**
+         * The associated {@link Class}.
+         */
         private Class<? extends Gate> gclass = null;
+        /**
+         * The constructor used to create a {@link Gate}.
+         */
         private Constructor<? extends Gate> constr = null;
+        /**
+         * The id of the {@link Gate}.
+         */
         private String gid = "";
+        /**
+         * The allowed inputs/outputs for this gate class.
+         */
         private int icount = 0, ocount = 0;
 
+        /**
+         * Creates a new GateInfo.
+         * 
+         * @param gCls
+         *            - the class for the gate
+         * @param id
+         *            - the id for the gate
+         * @param inputs
+         *            - the number of inputs the gate accepts, or
+         *            {@link GateSys#VARIABLE_COUNT} for any number of inputs.
+         * @param outputs
+         *            - the number of outputs the gate accepts, or
+         *            {@link GateSys#VARIABLE_COUNT} for any number of outputs.
+         */
         private GateInfo(Class<? extends Gate> gCls, String id, int inputs,
                 int outputs) {
             gclass = gCls;
@@ -88,6 +188,9 @@ public class GateSys {
             ocount = outputs;
         }
 
+        /**
+         * Returns a textual representation of this {@link GateInfo}.
+         */
         @Override
         public String toString() {
             return "class: " + gclass + ", id: " + gid + ", inCount: " + icount
@@ -95,9 +198,25 @@ public class GateSys {
         }
     }
 
+    /**
+     * An abstract class that provides the basis of a Gate.
+     * 
+     * @author Kenzie Togami
+     */
     public static abstract class Gate {
+        /**
+         * The inputs/outputs in this Gate.
+         */
         public List<PyPoint> inputs = null, outputs = null;
 
+        /**
+         * Creates a new {@link Gate} with the specified number of ins and outs.
+         * 
+         * @param ins
+         *            - the number of input points
+         * @param outs
+         *            - the number of output points.
+         */
         protected Gate(int ins, int outs) {
             inputs = new ArrayList<PyPoint>(ins);
             outputs = new ArrayList<PyPoint>(outs);
@@ -109,6 +228,10 @@ public class GateSys {
             }
         }
 
+        /**
+         * Performs the logic this gate does, using the input list and pushing
+         * the result to the output list.
+         */
         public void doLogic() {
             boolean res = doLogic_(inputs);
             for (PyPoint out : outputs) {
@@ -116,8 +239,19 @@ public class GateSys {
             }
         }
 
+        /**
+         * Performs the logic this gate does on the given input list, and
+         * returns the boolean result.
+         * 
+         * @param inputs
+         *            - a list of {@link PyPoint PyPoints} to use as input.
+         * @return the result of the logic being preformed on the input list.
+         */
         protected abstract boolean doLogic_(List<PyPoint> inputs);
 
+        /**
+         * Returns a textual representation of this {@link Gate}.
+         */
         @Override
         public String toString() {
             return classToGate.get(getClass()).gid + " [inputs: " + inputs
@@ -125,12 +259,25 @@ public class GateSys {
         }
     }
 
+    /**
+     * The implementation of an and gate.
+     * 
+     * @author Kenzie Togami
+     */
     private static class AndGate extends Gate {
 
+        /**
+         * @see Gate#Gate(int, int)
+         */
         protected AndGate(int ins, int outs) {
             super(ins, outs);
         }
 
+        /**
+         * This does an <i>and</i> over the list.<br>
+         * <br>
+         * {@inheritDoc}
+         */
         @Override
         protected boolean doLogic_(List<PyPoint> inputs) {
             boolean res = State.ON;
@@ -141,12 +288,25 @@ public class GateSys {
         }
     }
 
+    /**
+     * The implementation of an or gate.
+     * 
+     * @author Kenzie Togami
+     */
     private static class OrGate extends Gate {
 
+        /**
+         * @see Gate#Gate(int, int)
+         */
         protected OrGate(int ins, int outs) {
             super(ins, outs);
         }
 
+        /**
+         * This does an <i>or</i> over the list.<br>
+         * <br>
+         * {@inheritDoc}
+         */
         @Override
         protected boolean doLogic_(List<PyPoint> inputs) {
             boolean res = State.OFF;
@@ -158,6 +318,7 @@ public class GateSys {
     }
 
     static {
+        // this adds the default gates.
         addGate(AndGate.class, "andGate", VARIABLE_COUNT, 1);
         addGate(OrGate.class, "orGate", VARIABLE_COUNT, 1);
     }
