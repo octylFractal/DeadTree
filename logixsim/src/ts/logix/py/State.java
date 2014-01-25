75598d90be2d57b604e03b58c5e7a71a30906864
Kenzie Togami <ket1999@gmail.com>
2014-01-25 15:27:50 -0800

    Separate State vars

--- logixsim/src/ts/logix/py/State.java
+++ logixsim/src/ts/logix/py/State.java
@@ -2,7 +2,9 @@ package ts.logix.py;
 
 public class State {
 
-    public static final boolean OFF = false, ON = true, CONNECTED = true,
-            DISCONNECTED = false;
+    public static final boolean OFF = false;
+    public static final boolean ON = true;
+    public static final boolean CONNECTED = true;
+    public static final boolean DISCONNECTED = false;
 
 }
