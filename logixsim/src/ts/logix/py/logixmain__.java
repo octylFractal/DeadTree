6cded5abc11dedfb1785dcce42f8d145c93a354e
Kenzie Togami <ket1999@gmail.com>
2014-01-25 07:40:28 -0800

    Add one abstraction over a py file, more to come

---
+++ logixsim/src/ts/logix/py/logixmain__.java
@@ -0,0 +1,14 @@
+package ts.logix.py;
+
+import java.io.File;
+
+import k.core.util.jythonintegration.JythonFile;
+import k.core.util.jythonintegration.JythonIntergration;
+
+public class logixmain__ {
+    private static JythonFile file = JythonIntergration.getFile(new File(
+            "./python/logixmain__.py"));
+
+    private logixmain__() {
+    }
+}
