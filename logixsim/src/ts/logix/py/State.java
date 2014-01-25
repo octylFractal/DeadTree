4e1eee0b8b467cdecc8db4d7f0fb62e321d14f47
Kenzie Togami <ket1999@gmail.com>
2014-01-25 15:35:20 -0800

    Document State

--- logixsim/src/ts/logix/py/State.java
+++ logixsim/src/ts/logix/py/State.java
@@ -2,9 +2,21 @@ package ts.logix.py;
 
 public class State {
 
+    /**
+     * The value for a point that is OFF.
+     */
     public static final boolean OFF = false;
+    /**
+     * The value for a point that is ON.
+     */
     public static final boolean ON = true;
+    /**
+     * The value for a CONNECTED link.
+     */
     public static final boolean CONNECTED = true;
+    /**
+     * The value for DISCONNECTED link.
+     */
     public static final boolean DISCONNECTED = false;
 
 }
