1fbc5da6f9df7386c3800d55d95611e26a51a128
Kenzie Togami <ket1999@gmail.com>
2014-01-25 13:10:37 -0800

    Document Conversion

--- logixsim/src/ts/logix/py/Conversion.java
+++ logixsim/src/ts/logix/py/Conversion.java
@@ -1,8 +1,19 @@
 package ts.logix.py;
 
-
+/**
+ * The matching Python file is named Conversion.py.
+ * 
+ * @author Kenzie Togami
+ */
 public class Conversion {
 
+    /**
+     * Converts the given state boolean to a string.
+     * 
+     * @param state
+     *            - the state to convert
+     * @return "On" for <tt>true</tt>, "Off" for <tt>false</tt>.
+     */
     public static String getStateAsStr(boolean state) {
         return state ? "On" : "Off";
     }
