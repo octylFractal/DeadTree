ec8fef7e79a53d3995bcc80fc75ffd0f9f2152fc
Kenzie Togami <ket1999@gmail.com>
2014-01-25 20:49:52 -0800

    Remove old system

--- logixsim/src/ts/logix/file/LogixSystem.java
+++ logixsim/src/ts/logix/file/LogixSystem.java
@@ -12,53 +12,9 @@ import ts.logix.py.PointSys.PyPoint;
 public class LogixSystem {
     public static final String fileext = "lsys";
     private String id = "";
-    private GridObject[][] grid = new GridObject[0][0];
 
-    public LogixSystem(String name, int width, int height) {
+    public LogixSystem(String name) {
         id = name;
-        grid = new GridObject[width][height];
-    }
-
-    public PyPoint createPoint(int x, int y) {
-        PyPoint p = (PyPoint) grid[x][y];
-        if (p == null) {
-            p = new PyPoint();
-        }
-        grid[x][y] = p;
-        return p;
-    }
-
-    public PyPoint removePoint(int x, int y) {
-        PyPoint p = (PyPoint) grid[x][y];
-        grid[x][y] = null;
-        return p;
-    }
-
-    public PyPoint pointAt(int x, int y) {
-        return (PyPoint) grid[x][y];
-    }
-
-    public void connect(int x1, int y1, int x2, int y2) {
-        LogixUtil.connect(pointAt(x1, y1), pointAt(x2, y2));
-    }
-
-    public void disconnect(int x1, int y1, int x2, int y2) {
-        LogixUtil.disconnect(pointAt(x1, y1), pointAt(x2, y2));
-    }
-
-    public Gate createGate(int x, int y, String id, int inputs, int outputs) {
-        Gate g = (Gate) grid[x][y];
-        if (g == null) {
-            g = GateSys.newGate(id).setInputs(inputs).setOutputs(outputs)
-                    .build();
-        }
-        grid[x][y] = g;
-        return g;
-    }
-
-    public Gate putGate(int x, int y, Gate g) {
-        grid[x][y] = g;
-        return g;
     }
 
     /**
