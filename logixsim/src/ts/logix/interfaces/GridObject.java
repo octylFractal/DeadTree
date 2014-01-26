378fcfaaa1dd6541934530c1b01f14594630fa07
Kenzie Togami <ket1999@gmail.com>
2014-01-25 16:07:56 -0800

    Create GridObject interface

---
+++ logixsim/src/ts/logix/interfaces/GridObject.java
@@ -0,0 +1,10 @@
+package ts.logix.interfaces;
+
+/**
+ * An interface that marks this object as usable in a grid of other GridObjects.
+ * 
+ * @author Kenzie Togami
+ */
+public interface GridObject {
+
+}
